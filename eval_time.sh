5e673b4e5cba6f89c2b769cdcbdd8e3f8ba06cb4
horransun <horransun@tencent.com>
2025-12-21 02:27:55 +0800

    time

--- eval_time.sh
+++ eval_time.sh
@@ -4,24 +4,24 @@ PAPER_TABLE=coco2017_cap_val
 
 LOG_DIR=./logs_final
 
-RUN_NAME=divprune_llava_1.5_7b_time
+RUN_NAME=divprune_llava_1.5_13b_time
 CUDA_VISIBLE_DEVICES=0,1,2,3 BASELINE=OURS LAYER_INDEX=0 SUBSET_RATIO=0.098 EVAL_TIME=TRUE python3 -m accelerate.commands.launch \
     --num_processes=4 \
     -m lmms_eval \
     --model llava \
-    --model_args pretrained="liuhaotian/llava-v1.6-vicuna-7b" \
+    --model_args pretrained="liuhaotian/llava-v1.5-13b" \
     --tasks $PAPER_TABLE \
     --batch_size 1 \
     --log_samples \
     --log_samples_suffix $RUN_NAME \
     --output_path $LOG_DIR/$RUN_NAME | tee ./log_divprune.log
 
-RUN_NAME=orig_llava_1.5_7b_time
+RUN_NAME=orig_llava_1.5_13b_time
 CUDA_VISIBLE_DEVICES=0,1,2,3 BASELINE=ORIG EVAL_TIME=TRUE python3 -m accelerate.commands.launch \
     --num_processes=4 \
     -m lmms_eval \
     --model llava \
-    --model_args pretrained="liuhaotian/llava-v1.6-vicuna-7b" \
+    --model_args pretrained="liuhaotian/llava-v13b" \
     --tasks $PAPER_TABLE \
     --batch_size 1 \
     --log_samples \
